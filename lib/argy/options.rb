6dd5085d02d0fe1e5bad51dd23532285b43a0395
Ray Zane <raymondzane@gmail.com>
2019-12-10 17:45:35 -0500

    Make Argy::Options a little more strict

--- lib/argy/options.rb
+++ lib/argy/options.rb
@@ -19,17 +19,20 @@ module Argy
     private
 
     def respond_to_missing?(meth, *)
-      meth[-1] == "?" || @values.key?(meth) || super
+      @values.key?(meth.to_s.sub(/\?$/, "").to_sym) || super
     end
 
     def method_missing(meth, *args)
       query = meth[-1] == "?"
       key = query ? meth[0..-2].to_sym : meth.to_sym
 
-      return !!@values[key] if query
       return super unless @values.key?(key)
-      return @values[key] if args.empty?
-      raise ArgumentError, "wrong number of arguments (given #{args.length}, expected 0)"
+
+      unless args.empty?
+        raise ArgumentError, "wrong number of arguments (given #{args.length}, expected 0)"
+      end
+
+      query ? !!@values[key] : @values[key]
     end
   end
 end
--- spec/argy/options_spec.rb
+++ spec/argy/options_spec.rb
@@ -1,68 +1,103 @@
 require "argy/options"
 
 RSpec.describe Argy::Options do
-  it "reads options" do
-    opts = Argy::Options.new(value: 1)
-    expect(opts.value).to eq(1)
+  describe "reader methods" do
+    it "reads options" do
+      opts = Argy::Options.new(value: 1)
+      expect(opts.value).to eq(1)
+    end
+
+    it "raises NoMethodError for non-options" do
+      opts = Argy::Options.new({})
+      expect{ opts.value }.to raise_error(NoMethodError)
+    end
+
+    it "raises ArgumentError for too many arguments" do
+      opts = Argy::Options.new(value: 1)
+      expect { opts.value(:foo) }.to raise_error(ArgumentError)
+    end
   end
 
-  it "raises NoMethodError for non-options" do
-    opts = Argy::Options.new({})
-    expect{ opts.value }.to raise_error(NoMethodError)
+  describe "query methods" do
+    it "queries for truthy options" do
+      opts = Argy::Options.new(value: 1)
+      expect(opts.value?).to be(true)
+    end
+
+    it "queries for false options" do
+      opts = Argy::Options.new(value: false)
+      expect(opts.value?).to be(false)
+    end
+
+    it "queries for nil options" do
+      opts = Argy::Options.new(value: nil)
+      expect(opts.value?).to be(false)
+    end
+
+    it "raises a NoMethodError for non-options" do
+      opts = Argy::Options.new({})
+      expect { opts.value? }.to raise_error(NoMethodError)
+    end
+
+    it "raises an ArgumentError for too many arguments" do
+      opts = Argy::Options.new(value: 1)
+      expect { opts.value?(:foo) }.to raise_error(ArgumentError)
+    end
   end
 
-  it "raises ArgumentError for additional arguments" do
-    opts = Argy::Options.new(value: 1)
-    expect { opts.value(:foo) }.to raise_error(ArgumentError)
+  describe "#respond_to?" do
+    it "responds to option names" do
+      opts = Argy::Options.new(value: 1)
+      expect(opts.respond_to?(:value)).to be(true)
+    end
+
+    it "reponds to queries for option names" do
+      opts = Argy::Options.new(value: 1)
+      expect(opts.respond_to?(:value?)).to be(true)
+    end
+
+    it "does not respond to non-option names" do
+      opts = Argy::Options.new({})
+      expect(opts.respond_to?(:value)).to be(false)
+    end
+
+    it "does not respond to queries for non-option names" do
+      opts = Argy::Options.new({})
+      expect(opts.respond_to?(:value?)).to be(false)
+    end
   end
 
-  it "queries for truthy options" do
-    opts = Argy::Options.new(value: 1)
-    expect(opts.value?).to be(true)
+  describe "#[]" do
+    it "gets an option" do
+      opts = Argy::Options.new(value: 1)
+      expect(opts[:value]).to eq(1)
+    end
+
+    it "gets a non-option" do
+      opts = Argy::Options.new({})
+      expect(opts[:value]).to be_nil
+    end
   end
 
-  it "queries for nil options" do
-    opts = Argy::Options.new(value: nil)
-    expect(opts.value?).to be(false)
-  end
+  describe "#fetch" do
+    it "fetches an option" do
+      opts = Argy::Options.new(value: 1)
+      expect(opts.fetch(:value)).to eq(1)
+    end
 
-  it "queries for false options" do
-    opts = Argy::Options.new(value: false)
-    expect(opts.value?).to be(false)
-  end
+    it "fetches a non-option" do
+      opts = Argy::Options.new({})
+      expect { opts.fetch(:value) }.to raise_error(KeyError)
+    end
 
-  it "queries for non-options" do
-    opts = Argy::Options.new({})
-    expect(opts.value?).to be(false)
-  end
+    it "fetches an option with a default" do
+      opts = Argy::Options.new({})
+      expect(opts.fetch(:value, 1)).to eq(1)
+    end
 
-  it "reads an option with hash syntax" do
-    opts = Argy::Options.new(value: 1)
-    expect(opts[:value]).to eq(1)
-  end
-
-  it "reads an non-option with hash syntax" do
-    opts = Argy::Options.new({})
-    expect(opts[:value]).to be_nil
-  end
-
-  it "fetches an option" do
-    opts = Argy::Options.new(value: 1)
-    expect(opts.fetch(:value)).to eq(1)
-  end
-
-  it "fetches a non-option" do
-    opts = Argy::Options.new({})
-    expect { opts.fetch(:value) }.to raise_error(KeyError)
-  end
-
-  it "fetches an option with a default" do
-    opts = Argy::Options.new({})
-    expect(opts.fetch(:value, 1)).to eq(1)
-  end
-
-  it "fetches an option with a default block" do
-    opts = Argy::Options.new({})
-    expect(opts.fetch(:value) { 1 }).to eq(1)
+    it "fetches an option with a default block" do
+      opts = Argy::Options.new({})
+      expect(opts.fetch(:value) { 1 }).to eq(1)
+    end
   end
 end
